81cb1ac02c0e2de1563632241a3ded3782404ef3
s21291 <s21291@pjwstk.edu.pl>
2022-10-28 12:45:32 +0200

    autoincrement id

--- src/main/java/com/example/demo3/model/University.java
+++ src/main/java/com/example/demo3/model/University.java
@@ -3,10 +3,7 @@ package com.example.demo3.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.List;
 
 
@@ -16,6 +13,7 @@ import java.util.List;
 public class University {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
 
